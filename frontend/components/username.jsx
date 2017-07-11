1f8c22f9b42d1a0f6cb6fdfd481bf97405eb5ef4
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-07-11 16:51:52 -0700

    Fix form inputs for username

--- frontend/components/username.jsx
+++ frontend/components/username.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 class UsernameForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { username: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({ username: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('A username was submitted: ' + this.state.username);
   }
 
   render() {
@@ -14,7 +28,7 @@ class UsernameForm extends React.Component {
           <input type='text'
                 className='username' 
                 onChange={ this.handleChange } 
-                value={ this.state.formula } />
+                value={ this.state.username } />
           <input type="submit" 
                 value="Submit" 
                 className='username-submit'/>
